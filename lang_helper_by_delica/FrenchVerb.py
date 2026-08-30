dfdd24581ba3f5f96e7bde00b52aabddc506a10c
delicalm <stormindustries22@outlook.com>
2026-08-30 18:59:19 -0500

    Filling repository

--- lang_helper_by_delica/FrenchVerb.py
+++ lang_helper_by_delica/FrenchVerb.py
@@ -5,7 +5,8 @@ class FrenchVerb(Verb):
     def __init__(self, verb, english_def="", verb_type=BASIC_VERB_TYPE,
                  first_per_sing=None, second_per_sing=None, third_per_sing=None,
                  first_per_plur=None, second_per_plur=None, third_per_plur=None):
-        super().__init__(verb, verb_type)
+        super().__init__(verb, verb_type, first_per_sing, second_per_sing, third_per_sing,
+                         first_per_plur, second_per_plur, third_per_plur)
         self.eng = english_def
 
 
--- lang_helper_by_delica/FrenchVerbRegER.py
+++ lang_helper_by_delica/FrenchVerbRegER.py
@@ -1,50 +1,67 @@
 from lang_helper_by_delica.FrenchVerb import FrenchVerb
-from lang_constants import FIRST_PERSON, SECOND_PERSON, THIRD_PERSON, PERSON_OPTIONS
+from lang_constants import *
 
 class FrenchVerbRegER(FrenchVerb):
+    FIRST_PER_SING_END = "e"
+    SECOND_PER_SING_END = "es"
+    THIRD_PER_SING_END = "e"
+    FIRST_PER_PLUR_END = "ons"
+    SECOND_PER_PLUR_END = "ez"
+    THIRD_PER_PLUR_END = "ent"
     def __init__(self, verb, english_def=""):
-        super().__init__(verb, english_def)
+        assert len(verb) > 2
+        verb_root = verb[:,-2]
+        first_per_sing_form = verb_root + self.FIRST_PER_SING_END
+        second_per_sing_form = verb_root + self.SECOND_PER_SING_END
+        third_per_sing_form = verb_root + self.THIRD_PER_SING_END
+        first_per_plur_form = verb_root + self.FIRST_PER_PLUR_END
+        second_per_plur_form = verb_root + self.SECOND_PER_PLUR_END
+        third_per_plur_form = verb_root + self.THIRD_PER_PLUR_END
+        super().__init__(verb, english_def=english_def, verb_type=FRENCH_REG_ER_VERB_TYPE,
+                         first_per_sing=first_per_sing_form, second_per_sing=second_per_sing_form,
+                         third_per_sing=third_per_sing_form, first_per_plur=first_per_plur_form,
+                         second_per_plur=second_per_plur_form, third_per_plur=third_per_plur_form,)
 
-    def conjugate(self, person, is_plural=False):
-        assert len(self.verb) > 2
-        assert person in PERSON_OPTIONS
-        result = self.verb[:-2]
-        if not is_plural:
-            if person == FIRST_PERSON:
-                result += self.get_first_sing_person_ending()
-            elif person == SECOND_PERSON:
-                result += self.get_second_sing_person_ending()
-            else:
-                result += self.get_third_sing_person_ending()
-        else:
-            if person == FIRST_PERSON:
-                result += self.get_first_plur_person_ending()
-            elif person == SECOND_PERSON:
-                result += self.get_second_plur_person_ending()
-            else:
-                result += self.get_third_plur_person_ending()
-        return result
-
-    @staticmethod
-    def get_first_sing_person_ending():
-        return "e"
-
-    @staticmethod
-    def get_second_sing_person_ending():
-        return "es"
-
-    @staticmethod
-    def get_third_sing_person_ending():
-        return "e"
-
-    @staticmethod
-    def get_first_plur_person_ending():
-        return "ons"
-
-    @staticmethod
-    def get_second_plur_person_ending():
-        return "ez"
-
-    @staticmethod
-    def get_third_plur_person_ending():
-        return "ent"
+    # def conjugate(self, person, is_plural=False):
+    #     assert len(self.verb) > 2
+    #     assert person in PERSON_OPTIONS
+    #     result = self.verb[:-2]
+    #     if not is_plural:
+    #         if person == FIRST_PERSON:
+    #             result += self.get_first_sing_person_ending()
+    #         elif person == SECOND_PERSON:
+    #             result += self.get_second_sing_person_ending()
+    #         else:
+    #             result += self.get_third_sing_person_ending()
+    #     else:
+    #         if person == FIRST_PERSON:
+    #             result += self.get_first_plur_person_ending()
+    #         elif person == SECOND_PERSON:
+    #             result += self.get_second_plur_person_ending()
+    #         else:
+    #             result += self.get_third_plur_person_ending()
+    #     return result
+    #
+    # @staticmethod
+    # def get_first_sing_person_ending():
+    #     return FrenchVerbRegER.FIRST_PER_SING_END
+    #
+    # @staticmethod
+    # def get_second_sing_person_ending():
+    #     return FrenchVerbRegER.SECOND_PER_SING_END
+    #
+    # @staticmethod
+    # def get_third_sing_person_ending():
+    #     return FrenchVerbRegER.THIRD_PER_SING_END
+    #
+    # @staticmethod
+    # def get_first_plur_person_ending():
+    #     return FrenchVerbRegER.FIRST_PER_PLUR_END
+    #
+    # @staticmethod
+    # def get_second_plur_person_ending():
+    #     return FrenchVerbRegER.SECOND_PER_PLUR_END
+    #
+    # @staticmethod
+    # def get_third_plur_person_ending():
+    #     return FrenchVerbRegER.THIRD_PER_PLUR_END
