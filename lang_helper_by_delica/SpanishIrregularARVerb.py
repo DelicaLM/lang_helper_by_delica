3d8e75c643e34d8cd439a30df11241a830cf1062
delicalm <stormindustries22@outlook.com>
2026-09-10 18:35:42 -0500

    Filling repository

--- lang_helper_by_delica/SpanishIrregularARVerb.py
+++ lang_helper_by_delica/SpanishIrregularARVerb.py
@@ -1,11 +1,20 @@
-from lang_helper_by_delica.SpanishVerb import SpanishVerb
+from lang_helper_by_delica.SpanishVerbRegAR import SpanishVerbRegAR
 from lang_constants import *
 
-class SpanishIrregularVerb(SpanishVerb):
+class SpanishIrregularARVerb(SpanishVerbRegAR):
     def __init__(self, verb, english_def="", verb_type=SPANISH_IRREGULAR_VERB_TYPE,
                  first_per_sing=None, second_per_sing=None, third_per_sing=None,
                  first_per_plur=None, second_per_plur=None, third_per_plur=None):
-        super().__init__(verb, english_def=english_def, verb_type=verb_type, first_per_sing=first_per_sing,
-                         second_per_sing=second_per_sing, third_per_sing=third_per_sing, first_per_plur=first_per_plur,
-                         second_per_plur=second_per_plur, third_per_plur=third_per_plur)
-        self.eng = english_def
+        super().__init__(verb, english_def=english_def)
+        if first_per_sing is not None:
+            self.first_per_sing = first_per_sing
+        if second_per_sing is not None:
+            self.second_per_sing = second_per_sing
+        if third_per_sing is not None:
+            self.third_per_sing = third_per_sing
+        if first_per_plur is not None:
+            self.first_per_plur = first_per_plur
+        if second_per_plur is not None:
+            self.second_per_plur = second_per_plur
+        if third_per_plur is not None:
+            self.third_per_plur = third_per_plur
--- lang_helper_by_delica/spanish_verbs.py
+++ lang_helper_by_delica/spanish_verbs.py
@@ -1,4 +1,5 @@
 from lang_helper_by_delica.SpanishIrregularVerb import SpanishIrregularVerb
+from lang_helper_by_delica.SpanishIrregularARVerb import SpanishIrregularARVerb
 
 
 
@@ -8,4 +9,9 @@ ser = SpanishIrregularVerb("ser", "to be",
 ir = SpanishIrregularVerb("ir", "to go",
                           first_per_sing="voy", second_per_sing="vas", third_per_sing="va",
                           first_per_plur="vamos", second_per_plur="vais", third_per_plur="van",)
-ir.print_conjugations()
+dar = SpanishIrregularARVerb("dar", "to give", first_per_sing="doy", second_per_plur="dais")
+estar = SpanishIrregularARVerb("estar", "to be (temporary states, locations)",
+                               first_per_sing="estoy", second_per_sing="estás", third_per_sing="está",
+                               third_per_plur="están")
+
+estar.print_conjugations()
