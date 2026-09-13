ddb07ba54f1a156f4ee15e2a39253f377c995d95
delicalm <stormindustries22@outlook.com>
2026-09-13 14:15:06 -0500

    Filling repository

--- lang_helper_by_delica/Adjective.py
+++ lang_helper_by_delica/Adjective.py
@@ -1,27 +1,35 @@
 from lang_helper_by_delica.Word import Word
 
 class Adjective(Word):
-    def __init__(self, adj, english_def="", before_verb=True, fem_sing_form="", fem_plur_form="",
+    def __init__(self, adj : str = "adjective", english_def : str = "", before_verb : bool = True,
+                 is_invariable : bool = False, is_loan_word : bool = False, fem_sing_form="", fem_plur_form="",
                  masc_sing_form="", masc_plur_form=""):
         super().__init__(adj, english_def)
         self.adj = adj
         self.before_verb = before_verb
-        if fem_sing_form != "":
-            self.fem_sing_form = fem_sing_form
+        self.is_loan_word = is_loan_word
+        if is_invariable:
+            self.masc_sing_form = self.adj
+            self.fem_sing_form = self.adj
+            self.masc_plur_form = self.adj
+            self.fem_plur_form = self.adj
         else:
-            self.fem_sing_form = adj
-        if fem_plur_form != "":
-            self.fem_plur_form = fem_plur_form
-        else:
-            self.fem_plur_form = adj
-        if masc_sing_form != "":
-            self.masc_sing_form = masc_sing_form
-        else:
-            self.masc_sing_form = adj
-        if masc_plur_form != "":
-            self.masc_plur_form = masc_plur_form
-        else:
-            self.masc_plur_form = adj
+            if fem_sing_form != "":
+                self.fem_sing_form = fem_sing_form
+            else:
+                self.fem_sing_form = adj
+            if fem_plur_form != "":
+                self.fem_plur_form = fem_plur_form
+            else:
+                self.fem_plur_form = adj
+            if masc_sing_form != "":
+                self.masc_sing_form = masc_sing_form
+            else:
+                self.masc_sing_form = adj
+            if masc_plur_form != "":
+                self.masc_plur_form = masc_plur_form
+            else:
+                self.masc_plur_form = adj
 
 
 
--- lang_helper_by_delica/FrenchAdjective.py
+++ lang_helper_by_delica/FrenchAdjective.py
@@ -2,37 +2,44 @@ from lang_helper_by_delica.Adjective import Adjective
 
 
 class FrenchAdjective(Adjective):
-    def __init__(self, adj : str, english_def : str = "", before_verb : bool = False, masc_plur_form : str = "",
+    def __init__(self, adj : str, english_def : str = "", before_verb : bool = False, is_invariable : bool = False, masc_plur_form : str = "",
                  fem_sing_form : str ="", fem_plur_form : str = ""):
         masc_sing_form = adj
-        if fem_sing_form == "":
-            if adj.endswith("f"):
-                fem_sing_form = masc_sing_form[:-1] + "ve"
-            elif adj.endswith("x"):
-                fem_sing_form = masc_sing_form[:-1] + "se"
-            elif adj.endswith("el"):
-                fem_sing_form = masc_sing_form + "le"
-            elif adj.endswith("en") or adj.endswith("on"):
-                fem_sing_form = masc_sing_form + "ne"
-            elif adj.endswith("et"):
-                fem_sing_form = masc_sing_form + "te"
-            elif adj.endswith("e"):
-                fem_sing_form = masc_sing_form
-            elif adj.endswith("eau"):
-                fem_sing_form = masc_sing_form[:-2] + "lle"
-            else:
-                fem_sing_form = adj + "e"
-        if fem_plur_form == "":
-            fem_plur_form = fem_sing_form + "s"
-        if masc_plur_form == "":
-            if masc_sing_form.endswith("x") or masc_sing_form.endswith("s"):
-                masc_plur_form = masc_sing_form
-            elif adj.endswith("eau"):
-                masc_plur_form = masc_sing_form + "x"
-            elif adj.endswith("al"):
-                masc_plur_form = masc_sing_form[:-1] + "ux"
-            else:
-                masc_plur_form = masc_sing_form + "s"
+        if is_invariable:
+            masc_plur_form = masc_sing_form
+            fem_sing_form = masc_sing_form
+            fem_plur_form = masc_sing_form
+        else:
+            if fem_sing_form == "":
+                if adj.endswith("f"):
+                    fem_sing_form = masc_sing_form[:-1] + "ve"
+                elif adj.endswith("x"):
+                    fem_sing_form = masc_sing_form[:-1] + "se"
+                elif adj.endswith("el"):
+                    fem_sing_form = masc_sing_form + "le"
+                elif adj.endswith("en") or adj.endswith("on"):
+                    fem_sing_form = masc_sing_form + "ne"
+                elif adj.endswith("et"):
+                    fem_sing_form = masc_sing_form + "te"
+                elif adj.endswith("e"):
+                    fem_sing_form = masc_sing_form
+                elif adj.endswith("eau"):
+                    fem_sing_form = masc_sing_form[:-2] + "lle"
+                elif adj.endswith("eur"):
+                    fem_sing_form = masc_sing_form[:-1] + "se"
+                else:
+                    fem_sing_form = adj + "e"
+            if fem_plur_form == "":
+                fem_plur_form = fem_sing_form + "s"
+            if masc_plur_form == "":
+                if masc_sing_form.endswith("x") or masc_sing_form.endswith("s"):
+                    masc_plur_form = masc_sing_form
+                elif adj.endswith("eau"):
+                    masc_plur_form = masc_sing_form + "x"
+                elif adj.endswith("al"):
+                    masc_plur_form = masc_sing_form[:-1] + "ux"
+                else:
+                    masc_plur_form = masc_sing_form + "s"
         super().__init__(masc_sing_form, english_def=english_def, before_verb=before_verb, fem_sing_form=fem_sing_form,
                          fem_plur_form=fem_plur_form, masc_sing_form=masc_sing_form, masc_plur_form=masc_plur_form)
 
