3e93af3bcd56d40d80c73e5feb985d96ae20ba04
delicalm <stormindustries22@outlook.com>
2026-09-14 18:57:35 -0500

    Filling repository

--- lang_helper_by_delica/Adjective.py
+++ lang_helper_by_delica/Adjective.py
@@ -1,12 +1,12 @@
 from lang_helper_by_delica.Word import Word
 
 class Adjective(Word):
-    def __init__(self, adj : str = "adjective", english_def : str = "", before_verb : bool = True,
+    def __init__(self, adj : str = "adjective", english_def : str = "", before_noun : bool = True,
                  is_invariable : bool = False, is_loan_word : bool = False, fem_sing_form="", fem_plur_form="",
                  masc_sing_form="", masc_plur_form=""):
         super().__init__(adj, english_def)
         self.adj = adj
-        self.before_verb = before_verb
+        self.before_noun = before_noun
         self.is_invariable = is_invariable
         self.is_loan_word = is_loan_word
         if is_invariable:
--- lang_helper_by_delica/FrenchAdjective.py
+++ lang_helper_by_delica/FrenchAdjective.py
@@ -2,10 +2,13 @@ from lang_helper_by_delica.Adjective import Adjective
 
 
 class FrenchAdjective(Adjective):
-    def __init__(self, adj : str, english_def : str = "", before_verb : bool = False, is_invariable : bool = False,
+    def __init__(self, adj : str, english_def : str = "", before_noun : bool = False, is_invariable : bool = False,
                  is_loan_word : bool = False, masc_plur_form : str = "",
-                 fem_sing_form : str ="", fem_plur_form : str = ""):
+                 fem_sing_form : str ="", fem_plur_form : str = "", masc_sing_before_vowel : str = ""):
         masc_sing_form = adj
+        self.masc_sing_before_vowel = masc_sing_form
+        if masc_sing_before_vowel != "":
+            self.masc_sing_before_vowel = masc_sing_before_vowel
         if is_invariable:
             masc_plur_form = masc_sing_form
             fem_sing_form = masc_sing_form
@@ -41,7 +44,7 @@ class FrenchAdjective(Adjective):
                     masc_plur_form = masc_sing_form[:-1] + "ux"
                 else:
                     masc_plur_form = masc_sing_form + "s"
-        super().__init__(masc_sing_form, english_def=english_def, before_verb=before_verb, is_invariable=is_invariable,
+        super().__init__(masc_sing_form, english_def=english_def, before_noun=before_noun, is_invariable=is_invariable,
                          is_loan_word=is_loan_word, fem_sing_form=fem_sing_form, fem_plur_form=fem_plur_form,
                          masc_sing_form=masc_sing_form, masc_plur_form=masc_plur_form)
 
@@ -56,19 +59,23 @@ class FrenchAdjective(Adjective):
                     special_string += ", "
                 special_string += "loan word"
             print(special_string)
-        if self.before_verb:
-            print("Masculin Singulier:", self.masc_sing_form, "(e.g., Il est un", self.masc_sing_form, "homme.)")
+        if self.before_noun:
+            if self.masc_sing_before_vowel == self.masc_sing_form:
+                print("Masculin Singulier:", self.masc_sing_form, "(e.g., Il est un", self.masc_sing_form, "homme.)")
+            else:
+                print("Masculin Singulier:", f"{self.masc_sing_form}/{self.masc_sing_before_vowel}","(e.g., Il est un",
+                      self.masc_sing_form, "mari. Il est un", self.masc_sing_before_vowel, "homme.)")
         else:
             print("Masculin Singulier:", self.masc_sing_form, "(e.g., Il est un homme", f"{self.masc_sing_form}.)")
-        if self.before_verb:
+        if self.before_noun:
             print("Féminin Singulier:", self.fem_sing_form, "(e.g., Elle est une", self.fem_sing_form, "femme.)")
         else:
             print("Féminin Singulier:", self.fem_sing_form, "(e.g., Elle est une femme", f"{self.fem_sing_form}.)")
-        if self.before_verb:
+        if self.before_noun:
             print("Masculin Pluriel:", self.masc_plur_form, "(e.g., Ils sont des", self.masc_plur_form, "hommes.)")
         else:
             print("Masculin Pluriel:", self.masc_plur_form, "(e.g., Ils sont des hommes", f"{self.masc_plur_form}.)")
-        if self.before_verb:
+        if self.before_noun:
             print("Féminin Pluriel:", self.fem_plur_form, "(e.g., Elles sont des", self.fem_plur_form, "femmes.)")
         else:
             print("Féminin Pluriel:", self.fem_plur_form, "(e.g., Elles sont des femmes", f"{self.fem_plur_form}.)")
